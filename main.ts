051b0be8ada1a85f986d1ac6c0d79d35ddad6894
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-19 13:05:49 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -4,8 +4,9 @@
  * Created on: Oct 2023
  * This program ...
 */
-let randomNumberOne = randint(0, 99)
-let randomNumberTwo = randint(0, 99)
+
+const randomNumberOne = randint(0, 99)
+const randomNumberTwo = randint(0, 99)
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
@@ -23,7 +24,7 @@ input.onButtonPressed(Button.B, function () {
 input.onGesture(Gesture.Shake, function () {
   if (randomNumberOne > randomNumberTwo) {
     basic.showString('1st < 2nd')
-  }
+}
   else {
     basic.showString('1st > 2nd')
   }
