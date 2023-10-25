9b9b50e0d25cee232aded2a6a797f7ac7158afca
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-25 17:28:53 +0000

    typescript version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,18 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Ihor Chernyshev
+Created on: Oct 25
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import random
 
+randomNumberOne = random.randint(0, 99)
+randomNumberTwo = random.randint(0, 99)
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.was_pressed():
+        display.show(randomNumberOne)
