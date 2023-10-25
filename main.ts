b4bee0993833f15ffef6ac6ba9ac2466486e129d
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-25 13:27:10 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -5,20 +5,19 @@
  * This program ...
 */
 
-let randomNumberOne = randint(0, 99)
-let randomNumberTwo = randint(0, 99)
+const randomNumberOne = randint(0, 99)
+const randomNumberTwo = randint(0, 99)
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-  randomNumberOne = randomNumberOne + 0
-  basic.showString('#1 :' + (randomNumberOne).toString())
+  basic.showString('#1 :'); basic.showNumber(randomNumberOne)
   basic.showIcon(IconNames.Happy)
 })
 
 input.onButtonPressed(Button.B, function () {
-  basic.showString('#2 :' + (randomNumberTwo).toString())
+  basic.showString('#2 :'); basic.showNumber(randomNumberTwo)
   basic.showIcon(IconNames.Happy)
 })
 
