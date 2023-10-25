61b26932e041458002d1d78e77e3ec14905aa2a4
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-25 13:14:53 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -5,28 +5,28 @@
  * This program ...
 */
 
-const rno = randint(0, 99)
-const rnt = randint(0, 99)
+const randomNumberOne = randint(0, 99)
+const randomNumberTwo = randint(0, 99)
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-  basic.showString('# 1 :' + (rno).toString())
+  basic.showString('The temperature is:' + (randomNumberOne).toString() + 'K')
   basic.showIcon(IconNames.Happy)
 })
 
 input.onButtonPressed(Button.B, function () {
-  basic.showString('#2 :' + (rnt).toString())
+  basic.showString('#2 :' + (randomNumberTwo).toString())
   basic.showIcon(IconNames.Happy)
 })
 
 input.onGesture(Gesture.Shake, function () {
-  if (rno > rnt) {
-    basic.showString(rno.toString() + '>' + rnt.toString())
+  if (randomNumberOne > randomNumberTwo) {
+    basic.showString(randomNumberOne.toString() + '>' + randomNumberTwo.toString())
     basic.showIcon(IconNames.Sad)
   } else {
-    basic.showString(rno.toString() + '<' + rno.toString())
+    basic.showString(randomNumberOne.toString() + '<' + randomNumberTwo.toString())
     basic.showIcon(IconNames.Sad)
   }
 })
