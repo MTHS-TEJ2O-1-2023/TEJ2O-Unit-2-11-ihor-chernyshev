7be5f777adfc611451b22f6576c9e07f1582d6fe
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-26 16:34:27 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -16,17 +16,14 @@ display.clear()
 display.show(Image.HAPPY)
 
 while True:
-
     # shows #1
     if button_a.was_pressed():
         display.show("# 1 :" + str(randomNumberOne))
-    
     # shows #2
     if button_b.was_pressed():
         display.show("# 2 :" + str(randomNumberTwo))
-    
     # shows which number is bigger
-    if (randomNumberOne > randomNumberTwo):
+    if randomNumberOne > randomNumberTwo:
         display.show(str(randomNumberOne) + ">" + str(randomNumberTwo))
     else:
         display.show(str(randomNumberOne) + "<" + str(randomNumberTwo))
