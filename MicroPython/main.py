0f009e56c6833a6b59218aff5efa46f9e5f7f42d
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-26 16:32:20 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,12 +7,26 @@ This module is a Micro:bit MicroPython program
 from microbit import *
 import random
 
+# creating numbers
 randomNumberOne = random.randint(0, 99)
 randomNumberTwo = random.randint(0, 99)
 
+# setup
 display.clear()
 display.show(Image.HAPPY)
 
 while True:
+
+    # shows #1
     if button_a.was_pressed():
-        display.show(randomNumberOne)
+        display.show("# 1 :" + str(randomNumberOne))
+    
+    # shows #2
+    if button_b.was_pressed():
+        display.show("# 2 :" + str(randomNumberTwo))
+    
+    # shows which number is bigger
+    if (randomNumberOne > randomNumberTwo):
+        display.show(str(randomNumberOne) + ">" + str(randomNumberTwo))
+    else:
+        display.show(str(randomNumberOne) + "<" + str(randomNumberTwo))
